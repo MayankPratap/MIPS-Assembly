d5697a40959f84f7905638fbac7ad01fa190ec86
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-01-21 15:26:53 +0530

    Increment and decrement variables using if-else

---
+++ if-else1.c
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+addi $s1,$s1,10   # i=10
+addi $s2,$s2,4    # j=4
+
+bne $s1,$s2,ELSE   # branch if i!=j
+addi $s1,$s1,1    # i++
+
+# If if has executed we do not want else to execute
+
+j NEXT   # jump over else to NEXT label
+
+ELSE: addi $s2,$s2,-1   # else j--
+
+NEXT : add $s2,$s2,$s1
+
+li $v0,1   # Loading system call mode into register $v0
+
+add $a0,$s2,$zero  # Assigning $a0 value of $s2
+
+syscall   # Calling Operating system to perform operation of print
